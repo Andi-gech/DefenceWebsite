5d34a1f8dcc232505ebe5880628cf55b0a59eeb8
Haileamlak Derbie <amlakhaile@gmail.com>
2023-06-28 12:38:12 -0700

    Basic structure of Adminstration page done

--- src/Pages/Adminstrationpage.js
+++ src/Pages/Adminstrationpage.js
@@ -1,7 +1,82 @@
 import React from "react";
 
 function Adminstrationpage() {
-  return <div className="Adminstrationpage"></div>;
+  return <div className="Adminstrationpage">
+    
+    <h1>Hierarchy</h1> 
+    <div class="container">
+      <div class="column-container">
+        <h3>President Office</h3>
+        <div class="column">
+          <ul>
+            <li>About President Office</li>
+            <li>Legal service directorate</li>
+            <li>Women and community affairs</li>
+            <li>Plan, Information and budget directorate</li>
+            <li>ict infrastructure and information technology directorate</li>
+            <li>Public and international relation and alumni directorate</li>
+            <li>change and good governace directorate</li>
+            <li>Ethics and anti-corruption Directorate</li>
+            <li>Internal audit service directorate</li>
+          </ul>
+
+        </div>
+      </div>
+      <div class="column-container">
+        <h3>Adminstrative Vice President</h3>
+        <div class="column">
+        <ul>
+            <li>About President Office</li>
+            <li>Legal service directorate</li>
+            <li>Women and community affairs</li>
+            <li>Plan, Information and budget directorate</li>
+            <li>ict infrastructure and information technology directorate</li>
+            <li>Public and international relation and alumni directorate</li>
+            <li>change and good governace directorate</li>
+            <li>Ethics and anti-corruption Directorate</li>
+            <li>Internal audit service directorate</li>
+          </ul>
+
+        </div>
+      </div>
+      <div class="column-container">
+        <h3>Research and Technology transfer vp office</h3>
+        <div class="column">
+        <ul>
+            <li>About President Office</li>
+            <li>Legal service directorate</li>
+            <li>Women and community affairs</li>
+            <li>Plan, Information and budget directorate</li>
+            <li>ict infrastructure and information technology directorate</li>
+            <li>Public and international relation and alumni directorate</li>
+            <li>change and good governace directorate</li>
+            <li>Ethics and anti-corruption Directorate</li>
+            <li>Internal audit service directorate</li>
+          </ul>
+
+        </div>
+      </div>
+      <div class="column-container">
+        <h3>Academic VP office</h3>
+        <div class="column">
+        <ul>
+            <li>About President Office</li>
+            <li>Legal service directorate</li>
+            <li>Women and community affairs</li>
+            <li>Plan, Information and budget directorate</li>
+            <li>ict infrastructure and information technology directorate</li>
+            <li>Public and international relation and alumni directorate</li>
+            <li>change and good governace directorate</li>
+            <li>Ethics and anti-corruption Directorate</li>
+            <li>Internal audit service directorate</li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+    </div>
+   ;
 }
 
 export default Adminstrationpage;
