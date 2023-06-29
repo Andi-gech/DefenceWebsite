cc060a1f8c6a1019ffabf27ad7446b2455cd189b
fluffyweird <ets027912feng@aastu.edu.et>
2023-06-29 17:46:29 +0300

    done with downlaod Page

--- src/Pages/Downloadpage.js
+++ src/Pages/Downloadpage.js
@@ -1,8 +1,53 @@
 import React from "react";
-
+import "../downloadpage.css";
+import pdf from "../Assets/pdf.pdf";
+import logo from "../Assets/pdf.png";
 function Downloadpage() {
   return (
-    <div className="Downloadpage"> {/* write your code  here b/n divs */}</div>
+    <div className="Downloadpage">{
+      <div>
+      <h1>Downloads</h1>
+      <div>      
+      <a href={pdf} class="my-container">
+      <div class="item">          
+          <div class="icon">
+             <img src={logo} alt="Icon"/>
+          </div>
+          <div class="description">
+             <h3>Academic Calander</h3>
+               <p>2023 G.C Academic Calander</p>
+          </div>
+        </div>
+      </a></div>
+      <div>      
+      <a href={pdf} class="my-container">
+      <div class="item">          
+          <div class="icon">
+             <img src={logo} alt="Icon"/>
+          </div>
+          <div class="description">
+             <h3>Rules and Regulations</h3>
+               <p>2023 G.C </p>
+          </div>
+        </div>
+      </a></div>
+      <div>      
+      <a href= {pdf}class="my-container">
+      <div class="item">          
+          <div class="icon">
+             <img src={logo} alt="Icon"/>
+          </div>
+          <div class="description">
+             <h3>ADMISSION FORMS</h3>
+               <p></p>
+          </div>
+        </div>
+      </a></div>
+      
+        </div>
+        
+    }</div>
+  
   );
 }
 
