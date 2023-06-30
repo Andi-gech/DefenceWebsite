c6cd9ffa23b559fd3d4ed0a7b6584362c60ff0f8
Andi-gech <andi.fab23@gmail.com>
2023-06-30 15:22:38 +0300

    fix card

--- src/Pages/Aboutpages.js
+++ src/Pages/Aboutpages.js
@@ -1,41 +1,24 @@
 import React, { useEffect } from "react";
+import UseCollageFech from "../hooks/UseCollageFech";
+import { useParams } from "react-router-dom";
+import Loadingpage from "./Loadingpage";
+import UseUniversityFech from "../hooks/UseUniversity";
 
 function Aboutpages() {
-  return (
-    <div className="Aboutpages">
-      <div className="AboutContainer">
-        <h1>ABOUT US</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          condimentum felis sit amet elit accumsan, congue finibus eros euismod.
-          Nam congue diam eu porta varius. Integer vel lectus porta, lacinia est
-          vitae, posuere nibh. Curabitur sed gravida velit, in lacinia justo.
-          Sed libero mi, dictum non odio vitae, vestibulum sodales urna. Aliquam
-          sed molestie mi. Quisque quis sapien placerat, ornare odio id, varius
-          ligula. Vivamus pretium libero id purus aliquam tincidunt. Aenean
-          blandit sodales augue sed aliquet. Ut bibendum iaculis aliquam.
-          Quisque commodo congue tempus. Curabitur eu augue vel tortor aliquam
-          venenatis ac sed nunc. Suspendisse imperdiet augue in ante ultricies,
-          a tristique erat vulputate. In egestas justo mi, id suscipit metus
-          mollis in.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          condimentum felis sit amet elit accumsan, congue finibus eros euismod.
-          Nam congue diam eu porta varius. Integer vel lectus porta, lacinia est
-          vitae, posuere nibh. Curabitur sed gravida velit, in lacinia justo.
-          Sed libero mi, dictum non odio vitae, vestibulum sodales urna. Aliquam
-          sed molestie mi. Quisque quis sapien placerat, ornare odio id, varius
-          ligula. Vivamus pretium libero id purus aliquam tincidunt. Aenean
-          blandit sodales augue sed aliquet. Ut bibendum iaculis aliquam.
-          Quisque commodo congue tempus. Curabitur eu augue vel tortor aliquam
-          venenatis ac sed nunc. Suspendisse imperdiet augue in ante ultricies,
-          a tristique erat vulputate. In egestas justo mi, id suscipit metus
-          mollis in.
-        </p>
+  const { data, isLoading, isError } = UseUniversityFech();
+  if (data) {
+    return (
+      <div className="Aboutpages">
+        <div className="AboutContainer">
+          <h1>ABOUT US</h1>
+          <p>{data[0]?.about}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  if (isLoading) {
+    return <Loadingpage />;
+  }
 }
 
 export default Aboutpages;
