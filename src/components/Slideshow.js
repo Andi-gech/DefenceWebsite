475405d5cd425b32f6524e7e8421c07edfc539af
Andi-gech <andi.fab23@gmail.com>
2023-06-30 21:03:39 +0300

    fix card

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -51,7 +51,7 @@ function Slideshow({ id }) {
         style={{
           transform: isDesktopOrLaptop
             ? `translate3d(${-index * (images?.length || 0) * 2}%, 0, 0)`
-            : `translate3d(${-index * (images?.length || 0) * 10}%, 0, 0)`,
+            : `translate3d(${-index * (images?.length || 0) * 3}%, 0, 0)`,
         }}
       >
         {images?.map((department, idx) => (
