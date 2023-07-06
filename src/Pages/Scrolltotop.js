8314246e3206af5a6835ee7a7e5faa013f5335f5
Andi-gech <andi.fab23@gmail.com>
2023-07-07 01:09:17 +0300

    first

--- src/Pages/Scrolltotop.js
+++ src/Pages/Scrolltotop.js
@@ -5,10 +5,11 @@ function ScrollToTop() {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if (location.pathname.includes("Adminstration")) {
+      return;
+    } else {
+      window.scrollTo(0, 0);
+    }
   }, [location]);
-
-  return null;
 }
-
 export default ScrollToTop;
